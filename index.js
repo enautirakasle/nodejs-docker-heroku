f69e3e2f83f44a5e6dc470e27eed122b9101a7bd
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-04-05 12:38:08 +0200

    agurra visual studion

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World, nodejsn ejecutatzen nabil!')
+  response.send('Hello World, nodejsn ejecutatzen nabil!<br>hau da visual estudion windowsen egina')
 })
 
 app.listen(app.get('port'), function() {
